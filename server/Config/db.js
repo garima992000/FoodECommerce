fc4dc8ee5f5b8e5f4b2aefcce956f600107dfe17
garimasharma9916 <garimasharma9916@gmail.com>
2026-01-09 10:33:25 +0530

    upate

--- server/Config/db.js
+++ server/Config/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-
+//database connection
 mongoose.connect('mongodb+srv://admin:admin123@cluster0.h9fzijr.mongodb.net/food?appName=Cluster0')
 .then(()=>{console.log('MONGODB is Connected !!')})
 .catch((err)=>{console.log(err)})
